a70400d3b288ebc4a6b4a14d9b5ea1b7320e953c
yyk <yyk@mail.ru>
2021-04-07 11:14:21 -0400

    showing svg files from specific folder

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,17 @@ const {directory_filenames} = require('./get.js')
 const app = express();
 const port = 4000;
 const SOURCE_PATH = 'X:\\n\\images\\!svg\\screencasts\\screencast 2021-04-01.searchtides.link statuses.check';
+const  SVG_PATH= 'X:\\n\\images\\!svg'
 app.get('/', (request, response) => {
   const q = request.query;
   if (typeof q.id !== 'undefined') {
+    let final_path = SOURCE_PATH + '\\' + q.id + '.svg';
+    text = fs.readFileSync(final_path)
+    response.send('' + text);
   } else {
-  let xs = directory_filenames(SOURCE_PATH)
-  let ys = xs.map(x=> fs.readFileSync(SOURCE_PATH + '\\' + x));
-  let text = ys.join("<br>")
+    let xs = directory_filenames(SOURCE_PATH)
+    let ys = xs.map(x=> fs.readFileSync(SOURCE_PATH + '\\' + x));
+    let text = ys.join("<br>")
     response.send('' + text);
   }
 });
