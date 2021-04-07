21863eebd93ae1096d90b4414cd73410d083b8d8
yyk <yyk@mail.ru>
2021-04-06 23:12:01 -0400

    very simple viewer from illustrations in the given folder

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,15 @@ const fs = require('fs');
 const {directory_filenames} = require('./get.js')
 const app = express();
 const port = 4000;
-const SOURCE_PATH = 'X:\\n\\images\\!svg\\timeline';
-let xs = directory_filenames(SOURCE_PATH)
-console.log(xs.length)
+const SOURCE_PATH = 'X:\\n\\images\\!svg\\screencasts\\screencast 2021-04-01.searchtides.link statuses.check';
 app.get('/', (request, response) => {
   const q = request.query;
   if (typeof q.id !== 'undefined') {
   } else {
-    response.send('' + xs.length);
+  let xs = directory_filenames(SOURCE_PATH)
+  let ys = xs.map(x=> fs.readFileSync(SOURCE_PATH + '\\' + x));
+  let text = ys.join("<br>")
+    response.send('' + text);
   }
 });
 
